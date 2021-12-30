908fb4b2998b56affe54f8197724d77f4f1b5288
lakshmi-devi <lakshmi.d@access-designs.com>
2021-12-30 15:20:36 +0530

    creating a selenium project

---
+++ src/main/java/ManuAuto/manuf/App.java
@@ -0,0 +1,13 @@
+package ManuAuto.manuf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
